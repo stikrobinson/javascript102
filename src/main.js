fe4817e1c7be6856c00b6b784d04c485efdb1137
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-06-25 19:34:59 +0000

    taller

--- src/main.js
+++ src/main.js
@@ -23,3 +23,48 @@ const itemData = {
         score: 36
     }
 };
+
+const select = document.querySelector("select");
+
+select.innerHTML = "";
+for (let key of Object.keys(itemData)) {
+    const opcion = document.createElement("option");
+    opcion.value = key;
+    opcion.textContent = itemData[key].name;
+    select.appendChild(opcion);
+}
+
+function cargarDatos(dato) {
+    const display = document.getElementById("displayImage");
+    const fotografo = document.getElementById("photographer");
+    const descripcion = document.getElementById("description");
+    const puntaje = document.getElementById("score");
+
+    display.src = itemData[dato].image;
+    fotografo.value = itemData[dato].photographer;
+    descripcion.value = itemData[dato].description;
+    puntaje.value = itemData[dato].score;
+}
+
+cargarDatos(select.value = Object.keys(itemData)[0]);
+
+select.addEventListener("change", function() {
+    cargarDatos(select.value);
+});
+
+const incrementar = document.getElementById("increaseScore");
+const disminuir = document.getElementById("decreaseScore");
+
+incrementar.addEventListener("click", function() {
+    const dato = select.value;
+    itemData[dato].score += 1;
+    document.getElementById("score").value = itemData[dato].score;
+});
+
+disminuir.addEventListener("click", function() {
+    const dato = select.value;
+    itemData[dato].score -= 1;
+    document.getElementById("score").value = itemData[dato].score;
+});
+
+console.log("Llegue aqui!");
